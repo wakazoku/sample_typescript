8a4277611243e4bbb89a760415fcd1e16a2485fc
wakazoku <wakazoku@gmail.com>
2021-01-15 12:42:24 +0900

    配列型

---
+++ src/array.ts
@@ -0,0 +1,18 @@
+export {};
+
+let numbers = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings: Array<string> = ['tokyo', 'osaka', 'nagoya'];
+
+let twoDimentionArray: Array<Array<number>> = [
+  [50, 100],
+  [80, 100],
+];
+
+let twoDimentionArray2: number[][] = [
+  [50, 100],
+  [80, 100],
+];
+
+let twoDimentionArray3: (string | number | boolean)[] = [1, false, 'japan'];
