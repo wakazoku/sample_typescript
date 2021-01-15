f4cecfa07a15d908101fadbd1661b81d259b697e
wakazoku <wakazoku@gmail.com>
2021-01-15 16:06:52 +0900

    literal型

---
+++ src/literal.ts
@@ -0,0 +1,12 @@
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土' = '日';
+dayOfTheWeek = '月';
+// dayOfTheWeek = '13';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+// month = 14;
+
+let TURE: true = true;
+// TURE = false;
+
+export {};
