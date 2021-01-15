c07ca9b7f6b7fa6070b1e9cbace987730117616b
wakazoku <wakazoku@gmail.com>
2021-01-15 16:17:00 +0900

    enum型

---
+++ src/enum.ts
@@ -0,0 +1,36 @@
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Months.December);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLACK = '#000000',
+  // YELLOW = "#FFFF00"
+}
+
+console.log(COLORS.RED);
+
+// あとで追加できる
+enum COLORS {
+  YELLOW = '#FFFF00',
+}
+COLORS.YELLOW
+
+
+export {};
