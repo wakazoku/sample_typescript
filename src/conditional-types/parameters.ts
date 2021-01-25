3ea6dce6f493c00f2c3bb16827266baae03ffbe8
wakazoku <wakazoku@gmail.com>
2021-01-25 12:56:38 +0900

    parameters

---
+++ src/conditional-types/parameters.ts
@@ -0,0 +1,17 @@
+const debugProfile = (name: string, age: number) => {
+  console.log({ name, age });
+};
+
+debugProfile('taro', 22);
+
+// 引数の型を取り出す
+type Profile = Parameters<typeof debugProfile>; // type Profile = [string, number]
+
+const profile: Profile = ['jiro', 44];
+
+debugProfile(...profile);
+
+// type Parameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
+
+
+export {};
